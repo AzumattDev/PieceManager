da36999462772079a9d129004fc7daca96d130e6
sbtoonz <67915879+sbtoonz@users.noreply.github.com>
2022-02-19 22:20:19 -0700

    Submit MaterialReplacer.cs from RoloPogo as a form of substitute for mocking materials when making build pieces
    name your material in unity project _REPLACE_originalmaterialname
    
    and then call
    MaterialReplacer.GetAllMaterials();
    after creating your build Piece
    then call
    MaterialReplacer.ReplaceAllMaterialsWithOriginal(GameObject);

---
+++ MaterialReplacer.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections.Generic;
+using JetBrains.Annotations;
+using UnityEngine;
+
+namespace PieceManager
+{
+    [PublicAPI]
+    public static class MaterialReplacer
+    {
+        static MaterialReplacer()
+        {
+            originalMaterials = new Dictionary<string, Material>();
+        }
+        static Dictionary<string, Material> originalMaterials;
+        public static void GetAllMaterials()
+        {
+            var allmats = Resources.FindObjectsOfTypeAll<Material>();
+            foreach (var item in allmats)
+            {
+                originalMaterials[item.name] = item;
+            }
+        }
+
+        public static void ReplaceAllMaterialsWithOriginal(GameObject go)
+        {
+            if (originalMaterials == null) GetAllMaterials();
+
+            foreach (Renderer renderer in go.GetComponentsInChildren<Renderer>(true))
+            {
+                for (int i = 0; i < renderer.materials.Length; i++)
+                {
+                    if (renderer.materials[i].name.StartsWith("_REPLACE_"))
+                    {
+                        var matName = renderer.material.name.Replace(" (Instance)", string.Empty).Replace("_REPLACE_", "");
+
+                        if (originalMaterials.ContainsKey(matName))
+                        {
+                            renderer.material = originalMaterials[matName];
+                        }
+                        else
+                        {
+                            Debug.LogWarning("No suitable material found to replace: " + matName);
+                            // Skip over this material in future
+                            originalMaterials[matName] = renderer.material;
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
