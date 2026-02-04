6a8dc831c12e8b6ba99364ea0c2c7f55259f1f96
AzumattDev <azumattgaming@gmail.com>
2026-02-04 12:28:37 -0500

    PieceManager version 1.2.9

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -1,5 +1,6 @@
 ﻿using System.Reflection;
 using System.Runtime.InteropServices;
+using PieceManager;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
@@ -31,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.2.8")]
-[assembly: AssemblyFileVersion("1.2.8")]
+[assembly: AssemblyVersion(PieceManagerVersion.Version)]
+[assembly: AssemblyFileVersion(PieceManagerVersion.Version)]
